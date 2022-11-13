41e97b06257e151f95298091461c70a4b30e8418
Pietrell <diego.giuseppetti@gmail.com>
2022-11-13 22:12:33 +0100

    v0.1: disegna aereo

--- Project1/ElaboratoGiuseppetti.cpp
+++ Project1/ElaboratoGiuseppetti.cpp
@@ -20,7 +20,7 @@ float Tens = 0.0, Bias = 0.0, Cont = 0.0;  //Questa inizializzazione 'rappresent
 static unsigned int programId;
 mat4 Projection;
 GLuint MatProj, MatModel;
-unsigned int loc_time, loc_res, loc_mouse; 
+unsigned int loctime, locres, locmouse; 
 vec2 res;
 Figura Aereo, AereoPol;
 
@@ -39,9 +39,9 @@ void INIT_VAO(void)
 	//prendo da shader
 	MatProj = glGetUniformLocation(programId, "Projection");
 	MatModel = glGetUniformLocation(programId, "Model");
-	loc_time = glGetUniformLocation(programId, "time");
-	loc_res = glGetUniformLocation(programId, "res");
-	loc_mouse = glGetUniformLocation(programId, "mouse");
+	loctime = glGetUniformLocation(programId, "time");
+	locres = glGetUniformLocation(programId, "res");
+	locmouse = glGetUniformLocation(programId, "mouse");
 }
 
 void INIT_SHADER(void)
@@ -61,11 +61,15 @@ void INIT_SHADER(void)
 
 void costruisciAereo(Figura* fig, vec4 color) {
 
-	fig->vertex.push_back(vec3(1.0, 1.0, 0.0));
+	fig->vertex.push_back(vec3(0.0, 0.0, 0.0));
+	fig->colorVertex.push_back(color);
+	fig->vertex.push_back(vec3(0.0, 1.0, 0.0));
 	fig->colorVertex.push_back(color);
 	fig->vertex.push_back(vec3(-0.2, 0.0, 0.0));
 	fig->colorVertex.push_back(color);
-	fig->vertex.push_back(vec3(-1.0, -0.8, 0.0));
+	fig->vertex.push_back(vec3(-0.6, -0.8, 0.0));
+	fig->colorVertex.push_back(color);
+	fig->vertex.push_back(vec3(-0.2, -0.8, 0.0));
 	fig->colorVertex.push_back(color);
 	fig->vertex.push_back(vec3(-0.2, -1.0, 0.0));
 	fig->colorVertex.push_back(color);
@@ -73,11 +77,19 @@ void costruisciAereo(Figura* fig, vec4 color) {
 	fig->colorVertex.push_back(color);
 	fig->vertex.push_back(vec3(0.2, -0.8, 0.0));
 	fig->colorVertex.push_back(color);
-	fig->vertex.push_back(vec3(1.0, -0.8, 0.0));
+	fig->vertex.push_back(vec3(0.6, -0.8, 0.0));
 	fig->colorVertex.push_back(color);
-	fig->vertex.push_back(vec3(1.0, 1.0, 0.0));
+	fig->vertex.push_back(vec3(0.2, 0.0, 0.0));
 	fig->colorVertex.push_back(color);
+	fig->vertex.push_back(vec3(0.0, 1.0, 0.0));
+	fig->colorVertex.push_back(color);
+
 	fig->nv = fig->vertex.size();
+
+
+	fig->Model = mat4(1.0);
+	fig->Model = translate(fig->Model, vec3(w/2, h/2, 0.0));
+	fig->Model = scale(fig->Model, vec3(w/5, h/5, 1.0));
 	
 }
 
@@ -86,23 +98,26 @@ void costruisciAereo(Figura* fig, vec4 color) {
 
 void drawScene(void)
 {
-	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClearColor(0.2, 0.0, 0.0, 0.5);
 	glClear(GL_COLOR_BUFFER_BIT);
+	float time = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
+	vec2 resolution = vec2((float)w, (float)h);
+	glUseProgram(programId);
+	glUniformMatrix4fv(MatProj, 1, GL_FALSE, value_ptr(Projection));	
+	glUniform2f(locres, resolution.x, resolution.y);
 
 	vec4 color = vec4(1.0,1.0,1.0,1.0);
 	costruisciAereo(&Aereo,color);
+	//costruisci_formaHermite(color, &Aereo, &AereoPol);	
+	crea_VAO_Static(&Aereo);
 
-	//costruisci_formaHermite(color, &Aereo, &AereoPol);
-
-	glUniformMatrix4fv(MatProj, 1, GL_FALSE, value_ptr(Projection));
-	glUniform2f(loc_res, res.x, res.y);
 
 	
-	crea_VAO_Static(&Aereo);
+	glUniformMatrix4fv(MatModel, 1, GL_FALSE, value_ptr(Aereo.Model));
 	glBindVertexArray(Aereo.VAO);
-	glDrawArrays(GL_LINE_STRIP, 0, Aereo.vertex.size());
+	glDrawArrays(GL_TRIANGLE_FAN, 0, Aereo.nv);
 	glBindVertexArray(0);
-
+	
 
 
 	glutSwapBuffers();
@@ -115,18 +130,14 @@ int main(int argc, char* argv[])
 
 	glutInitContextVersion(4, 0);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
-
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-
 	glutInitWindowSize(w, h);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Editor Curve");
+	glutCreateWindow("Elaborato Giuseppetti");
 	glutDisplayFunc(drawScene);
 	//gestione animazione
 	//glutMouseFunc(onMouse);
 	//glutKeyboardFunc(mykeyboard);
-
-
 	glewExperimental = GL_TRUE;
 	glewInit();
 	INIT_SHADER();
